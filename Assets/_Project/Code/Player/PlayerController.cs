5732d81b37544f7bf459103d40adf2bfc5bc3cd9
Nejat Alper Satiroglu <n.satiroglu265@mybvc.ca>
2026-07-07 15:30:27 -0600

    Ground Check Implement

--- Assets/_Project/Code/Player/PlayerController.cs
+++ Assets/_Project/Code/Player/PlayerController.cs
@@ -1,26 +1,34 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    #region References
     MyInputActions _inputActions;
     private Rigidbody _rb;
-    
-    [SerializeField] LayerMask groundLayer;
+    #endregion
+
+    #region Serialized Fields
+    [Header("Movement Settings")]
     [SerializeField] private float moveSpeed = 5f;
     [SerializeField] private float jumpForce = 5f;
+    [Header("Ground Check Settings")]
+    [SerializeField] LayerMask groundLayer;
+    [SerializeField] private float maxDistance = 0.5f;
+    #endregion
     
+    #region Private Fields
     private float _movementInput;
     private Vector3 _movement;
     private bool _shouldJump;
+    #endregion
     
-
     private void Awake()
     {
         _inputActions = new MyInputActions();
         _rb = GetComponent<Rigidbody>();
     }
-
     private void OnEnable()
     {
         _inputActions.Enable();
@@ -28,7 +36,6 @@ public class PlayerController : MonoBehaviour
         _inputActions.Player.Movement.canceled += OnMoveCanceled;
         _inputActions.Player.Jump.performed += OnJumpPerformed;
     }
-
     private void OnDisable()
     {
         _inputActions.Disable();
@@ -36,7 +43,6 @@ public class PlayerController : MonoBehaviour
         _inputActions.Player.Movement.canceled -= OnMoveCanceled;
         _inputActions.Player.Jump.performed -= OnJumpPerformed;
     }
-
     private void FixedUpdate()
     {
         Move();
@@ -56,7 +62,6 @@ public class PlayerController : MonoBehaviour
             
         }
     }
-
     private void Move()
     {
         _rb.AddForce(moveSpeed * _movement , ForceMode.Force);
@@ -66,24 +71,20 @@ public class PlayerController : MonoBehaviour
         _rb.AddForce(jumpForce * Vector3.up, ForceMode.Impulse);
         _shouldJump = false;
     }
-
     private bool IsGrounded()
     {
         Ray ray = new Ray(transform.position, Vector3.down);
-        return Physics.SphereCast(ray, 0.1f, 0.5f,groundLayer);
+        return Physics.SphereCast(ray, 0.2f, maxDistance,groundLayer);
     }
-
     void OnMovePerformed(InputAction.CallbackContext context)
     {
         _movementInput = context.ReadValue<float>();
         _movement = new Vector3(_movementInput, 0, 0);
     }
-
     void OnMoveCanceled(InputAction.CallbackContext context)
     {
         _movement = Vector3.zero;
     }
-
     void OnJumpPerformed(InputAction.CallbackContext context)
     {
         _shouldJump = true;
